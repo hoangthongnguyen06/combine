2cc5ef258acad46a491081779bb7d81812ee9bfe
hoangthongnguyen06 <nguyenhoangthong06@gmail.com>
2024-07-13 14:51:40 +0700

    update

--- app/services/topic.py
+++ app/services/topic.py
@@ -1,29 +1,30 @@
 import requests
 from datetime import datetime
+from uuid import UUID
 from app.models import Topic
 from app import db
+
+payload = {
+    "page": 1,
+    "size": 1000,
+    "data": {
+        "name": "",
+        "organization_id": "null",
+        "only_root": "true"
+    },
+    "order_field": "created_at",
+    "order_type": "DESC"
+}
 
 def update_topics_from_api(api_url, headers=None, app=None):
     with app.app_context():
         try:
-            payload = {
-                "page": 1,
-                "size": 1000,
-                "data": {
-                    "name": "",
-                    "organization_id": None,
-                    "only_root": True
-                },
-                "order_field": "created_at",
-                "order_type": "DESC"
-            }
-
             response = requests.post(api_url, json=payload, headers=headers, verify=False)
             if response.status_code == 200:
                 api_data = response.json().get("data", {}).get("items", [])
                 for item in api_data:
                     topic_data = {
-                        "id": item["id"],
+                        "id": UUID(item["id"]),  # Chuyển đổi item["id"] thành UUID
                         "name": item["name"],
                         "date": datetime.strptime(item["start_at"], "%Y/%m/%d %H:%M:%S"),
                         "parent_id": item.get("topic_parent_id")
@@ -35,7 +36,7 @@ def update_topics_from_api(api_url, headers=None, app=None):
                             topic_data["status"] = "deactive"
                         else:
                             topic_data["status"] = "active"
-
+                    
                     topic = Topic.query.filter_by(id=topic_data['id']).first()
                     if topic:
                         for key, value in topic_data.items():
@@ -43,7 +44,7 @@ def update_topics_from_api(api_url, headers=None, app=None):
                     else:
                         topic = Topic(**topic_data)
                         db.session.add(topic)
-
+                    
                     db.session.commit()
                     print(f"Updated topic: {topic_data}")
             else:
@@ -52,4 +53,3 @@ def update_topics_from_api(api_url, headers=None, app=None):
             print(f"An error occurred with the request: {e}")
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
-
