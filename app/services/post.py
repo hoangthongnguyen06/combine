ec8dd8066ceb99377051484f19b00883a68466bb
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 <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>
2024-07-13 14:51:17 +0700

    [add] post service

--- app/services/post.py
+++ app/services/post.py
@@ -1,40 +1,114 @@
 import requests
 from app.models import Post
 from app import db
+from datetime import datetime
+from run import app
 
-def update_posts_from_api(api_url, headers=None):
-    response = requests.get(api_url, headers=headers)
-    if response.status_code == 200:
-        posts_data = response.json()
-        for post_data in posts_data:
-            post = Post.query.filter_by(id=post_data['id']).first()
-            if post:
-                post.update(**post_data)
-            else:
-                Post.create(**post_data)
-    else:
-        print(f"Failed to fetch posts: {response.status_code}")
+payload = {
+    "trends": [],
+    "keywords": [],
+    "sources": [
+        1,
+        2,
+        4,
+        5,
+        8,
+        9,
+        12
+    ],
+    "sentiments": [],
+    "domains": [],
+    "author_names": [],
+    "author_ids": [],
+    "wall_ids": [],
+    "article_types": [],
+    "date_from": "2024/07/13 00:00:00",
+    "date_to": "2024/07/13 23:59:59",
+    "pinned": "null",
+    "get_snippet": "true",
+    "order": 3,
+    "similar_master": 0,
+    "is_spam": "false",
+    "flow_ids": [],
+    "topic_ids": [],
+    "profile_group_id": "null",
+    "page": 0,
+    "size": 5000
+}
 
-def create_post(data):
-    post = Post.create(**data)
-    return post
 
-def update_post(id, data):
-    post = Post.query.get(id)
-    if post:
-        post.update(**data)
-        return post
-    return None
+def update_posts_from_api(api_url, headers=None, app=None):
+    with app.app_context():
+        response = requests.post(api_url, json=payload,
+                                 headers=headers, verify=False)
+        if response.status_code == 200:
+            api_data = response.json()
+            print(api_data)
+            for hit in api_data["data"]["hits"]:
+                post_data = {
+                    "id": hit["id"],
+                    "account_id": hit["author_id"],
+                    "content": hit["content"],
+                    "likes": hit["like_count"],
+                    "comments": hit["comment_count"],
+                    "shares": hit["share_count"],
+                    "domain": hit["domain"],
+                    "link": hit["url"],
+                    "type": hit["article_type"],
+                    "description": hit["description"],
+                }
+                # Xử lý sắc thái
+                if hit.get('sentiment') == -1:
+                    post_data["nuance"] = 'Tiêu cực'
+                elif hit.get('sentiment') == 0:
+                    post_data["nuance"] = 'Trung tính'
+                elif hit.get('sentiment') == 1:
+                    post_data["nuance"] = 'Tích cực'
+                # Xử lý hashtag
+                if hit.get('keywords'):
+                    post_data["hashtag"] = ', '.join(hit.get('keywords'))
+                # Xử lý
+                if hit.get('published_timestamp'):
+                    dt_object = datetime.fromtimestamp(
+                        hit['published_timestamp'] / 1000.0)
+                    post_data['date'] = dt_object.strftime("%Y/%m/%d %H:%M:%S")
+                top = Post.query.filter_by(id=post_data['id']).first()
+                if top:
+                    for key, value in post_data.items():
+                        setattr(top, key, value)
+                    db.session.commit()
+                else:
+                    new_obj = Post(**post_data)
+                    db.session.add(new_obj)
+                    db.session.commit()
+        else:
+            print(f"Failed to fetch posts: {response.status_code}")
 
-def delete_post(id):
-    post = Post.query.get(id)
-    if post:
-        post.delete()
-        return True
-    return False
 
-def get_post(id):
-    return Post.query.get(id)
+# def create_post(data):
+#     post = Post.create(**data)
+#     return post
 
-def get_all_posts():
-    return Post.query.all()
+
+# def update_post(id, data):
+#     post = Post.query.get(id)
+#     if post:
+#         post.update(**data)
+#         return post
+#     return None
+
+
+# def delete_post(id):
+#     post = Post.query.get(id)
+#     if post:
+#         post.delete()
+#         return True
+#     return False
+
+
+# def get_post(id):
+#     return Post.query.get(id)
+
+
+# def get_all_posts():
+#     return Post.query.all()
--- app/utils/scheduler.py
+++ app/utils/scheduler.py
@@ -1,7 +1,7 @@
 import requests
 from apscheduler.schedulers.background import BackgroundScheduler
 # from app.services.target import update_targets_from_api
-# from app.services.post import update_posts_from_api
+from app.services.post import update_posts_from_api
 from app.services.topic import update_topics_from_api
 # from app.services.object import update_objects_from_api
 # from app.services.result import update_results_from_api
@@ -11,9 +11,11 @@ from app import create_app
 import urllib3
 import certifi
 from urllib3.exceptions import InsecureRequestWarning
+# from app.services.post import update_posts_from_api
 
 # Bỏ qua cảnh báo không xác minh yêu cầu không an toàn
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
 
 def get_access_token(username, password):
     login_url = endpoints.APIPlatformEndpoints.LOGIN.value
@@ -22,30 +24,36 @@ def get_access_token(username, password):
         'password': password,
     }
     response = requests.post(login_url, json=login_data, verify=False)
-    
+
     if response.status_code == 200:
         access_token = response.json().get('data', {}).get('data', {}).get('token')
         print("Successfully obtained access token.")
         return access_token
     else:
-        print(f"Failed to obtain access token. Status code: {response.status_code}")
+        print(
+            f"Failed to obtain access token. Status code: {response.status_code}")
         return None
 
 
 def start_scheduler(app):
     scheduler = BackgroundScheduler()
-    
+
     with app.app_context():
         # Get access token
-        access_token = get_access_token(config.Config.USERNAME_PLATFORM, config.Config.PASSWORD_PLATFORM)
+        access_token = get_access_token(
+            config.Config.USERNAME_PLATFORM, config.Config.PASSWORD_PLATFORM)
         if not access_token:
             print("Failed to start scheduler. Could not obtain access token.")
             return
-        
+
         headers = {"Authorization": f"Bearer {access_token}"}
         # Schedule jobs with updated functions
         try:
-            scheduler.add_job(func=update_topics_from_api, trigger="interval", minutes=0.5, args=(endpoints.APIPlatformEndpoints.GET_TOPIC.value, headers, app))
+            # scheduler.add_job(func=update_topics_from_api, trigger="interval", minutes=0.5, args=(
+            #     endpoints.APIPlatformEndpoints.GET_TOPIC.value, headers, app))
+            scheduler.add_job(func=update_posts_from_api, trigger="interval", minutes=0.5, args=(
+                endpoints.APIPlatformEndpoints.POST.value, headers, app))
+
             scheduler.start()
             print("Scheduler started successfully.")
         except Exception as e:
