190e485a0503145d9aec430768328dec9815b520
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 <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>
2024-07-19 14:10:19 +0700

    [add] spyder topic new service

--- app/services/topic_day.py
+++ app/services/topic_day.py
@@ -5,18 +5,21 @@ from app import db
 from datetime import datetime
 import uuid
 from app.models.topic_new import Topic_new
-results = db.session.query(Topic_new.uid, Topic_new.name).filter(
+from app.config import Config
+results_platform = db.session.query(Topic_new.uid, Topic_new.name).filter(
     Topic_new.system == 'platform').all()
 # page = 66
+results_spyder = db.session.query(Topic_new.uid, Topic_new.name).filter(
+    Topic_new.system == 'spyder').all()
 
 
 def update_topic_day_from_api(api_url, headers=None, app=None):
     # Tạo ngày hôm qua và hôm nay
-    today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-    yesterday = today - timedelta(days=1)
+    # today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    # yesterday = today - timedelta(days=1)
 
     # Cập nhật payload với thời gian tương ứng
-    payload = {
+    payload_platform = {
         "date_from": "2024/01/01 00:00:00",
         "date_to": "2024/07/19 23:59:59",
         "sources": [
@@ -29,86 +32,126 @@ def update_topic_day_from_api(api_url, headers=None, app=None):
             12
         ]
     }
-    payload["date_from"] = yesterday.strftime("%Y/%m/%d 00:00:00")
-    payload["date_to"] = today.strftime("%Y/%m/%d 23:59:59")
+
+    # payload["date_from"] = yesterday.strftime("%Y/%m/%d 00:00:00")
+    # payload["date_to"] = today.strftime("%Y/%m/%d 23:59:59")
+
+    payload_spyder = {
+        "time_range": {
+            "from": "2024-01-01 00:00:00",
+            "to": "2024-01-01 23:59:59"
+        },
+        "category_id": 16064,
+        "top": 5,
+        "Topics": [
+            -1
+        ]
+    }
+
+    start_date = datetime.strptime("2024-01-01", "%Y-%m-%d")
+    end_date = datetime.strptime("2024-07-19", "%Y-%m-%d")
+
+    current_date = start_date
+
+    while current_date <= end_date:
+        date_str = current_date.strftime("%Y-%m-%d")
+        payload_spyder["time_range"]["from"] = f"{date_str} 00:00:00"
+        payload_spyder["time_range"]["to"] = f"{date_str} 23:59:59"
+
+        current_date += timedelta(days=1)
 
     with app.app_context():
-        for topic_id, topic_name in results:
-            payload["topic_ids"] = [topic_id]
-            try:
-                response = requests.post(api_url, json=payload,
+        if Config.PLATFORM_API_URL in api_url:
+            for topic_id, topic_name in results_platform:
+                payload_platform["topic_ids"] = [topic_id]
+                try:
+                    response = requests.post(api_url, json=payload_platform,
+                                             headers=headers, verify=False)
+                    if response.status_code == 200:
+                        for details_data in response["data"]:
+                            if (details_data["data"]):
+                                for details_data_1 in details_data["data"]:
+                                    extracted_data = {
+                                        "uid": str(uuid.uuid4()),
+                                        "id_topic": topic_id,
+                                        "topic_name": topic_name,
+                                        "date": details_data["date"],
+                                        "sum_of_posts": details_data_1["total_count"],
+                                        "positive_posts": details_data_1["positive_count"],
+                                        "neural_posts": details_data_1["neutral_count"],
+                                        "negative_posts": details_data_1["negative_count"],
+                                        "system": "platform"
+                                    }
+                                    # Xu ly nen tang cua tin bai
+                                    if details_data_1["source_id"] == 1:
+                                        extracted_data["platform"] = "Báo chí"
+                                    elif details_data_1["source_id"] == 2:
+                                        extracted_data["platform"] = "Trang điện tử"
+                                    elif details_data_1["source_id"] == 4:
+                                        extracted_data["platform"] = "Blog"
+                                    elif details_data_1["source_id"] == 5:
+                                        extracted_data["platform"] = "Diễn đàn"
+                                    elif details_data_1["source_id"] == 8:
+                                        extracted_data["platform"] = "Facebook"
+                                    elif details_data_1["source_id"] == 9:
+                                        extracted_data["platform"] = "Youtube"
+                                    elif details_data_1["source_id"] == 12:
+                                        extracted_data["platform"] = "Tiktok"
+                                    top = Post.query.filter_by(
+                                        id=extracted_data['id']).first()
+                                    if top:
+                                        for key, value in extracted_data.items():
+                                            setattr(top, key, value)
+                                        db.session.commit()
+                                    else:
+                                        new_obj = Post(**extracted_data)
+                                        db.session.add(new_obj)
+                                        db.session.commit()
+                    else:
+                        print(
+                            f"Failed to fetch objects: {response.status_code}")
+                except requests.exceptions.RequestException as e:
+                    print(f"An error occurred with the request: {e}")
+                except Exception as e:
+                    print(f"An unexpected error occurred: {e}")
+        elif Config.SPYDER_API_URL in api_url:
+            for topic_id, topic_name in results_spyder:
+                payload_spyder["category_id"] = topic_id
+                api_data = requests.post(api_url, json=payload_spyder,
                                          headers=headers, verify=False)
-                if response.status_code == 200:
-                    for details_data in response["data"]:
-                        if (details_data["data"]):
-                            for details_data_1 in details_data["data"]:
-                                extracted_data = {
-                                    "uid": str(uuid.uuid4()),
-                                    "id_topic": topic_id,
-                                    "topic_name": topic_name,
-                                    "date": details_data["date"],
-                                    "sum_of_posts": details_data_1["total_count"],
-                                    "positive_posts": details_data_1["positive_count"],
-                                    "neural_posts": details_data_1["neutral_count"],
-                                    "negative_posts": details_data_1["negative_count"],
-                                    "system": "platform"
-                                }
-                                # Xu ly nen tang cua tin bai
-                                if details_data_1["source_id"] == 1:
-                                    extracted_data["platform"] = "Báo chí"
-                                elif details_data_1["source_id"] == 2:
-                                    extracted_data["platform"] = "Trang điện tử"
-                                elif details_data_1["source_id"] == 4:
-                                    extracted_data["platform"] = "Blog"
-                                elif details_data_1["source_id"] == 5:
-                                    extracted_data["platform"] = "Diễn đàn"
-                                elif details_data_1["source_id"] == 8:
-                                    extracted_data["platform"] = "Facebook"
-                                elif details_data_1["source_id"] == 9:
-                                    extracted_data["platform"] = "Youtube"
-                                elif details_data_1["source_id"] == 12:
-                                    extracted_data["platform"] = "Tiktok"
-                                top = Post.query.filter_by(
-                                    id=extracted_data['id']).first()
-                                if top:
-                                    for key, value in extracted_data.items():
-                                        setattr(top, key, value)
-                                    db.session.commit()
-                                else:
-                                    new_obj = Post(**extracted_data)
-                                    db.session.add(new_obj)
-                                    db.session.commit()
-                else:
-                    print(f"Failed to fetch objects: {response.status_code}")
-            except requests.exceptions.RequestException as e:
-                print(f"An error occurred with the request: {e}")
-            except Exception as e:
-                print(f"An unexpected error occurred: {e}")
+                if api_data:
+                    result_data = api_data["result"]
+                    extracted_data = {
+                        "uid": str(uuid.uuid4()),
+                        "id_topic": topic_id,
+                        "topic_name": topic_name,
+                        "date": result_data["date"],
+                        "sum_of_posts": result_data["total_post"],
+                        "positive_posts": result_data["total_positive"],
+                        "neural_posts": result_data["total_neutral"],
+                        "negative_posts": result_data["total_negative"],
+                        "system": "spyder"
+                    }
+            # def create_post(data):
+            #     post = Post.create(**data)
+            #     return post
 
-# def create_post(data):
-#     post = Post.create(**data)
-#     return post
+            # def update_post(id, data):
+            #     post = Post.query.get(id)
+            #     if post:
+            #         post.update(**data)
+            #         return post
+            #     return None
 
+            # def delete_post(id):
+            #     post = Post.query.get(id)
+            #     if post:
+            #         post.delete()
+            #         return True
+            #     return False
 
-# def update_post(id, data):
-#     post = Post.query.get(id)
-#     if post:
-#         post.update(**data)
-#         return post
-#     return None
+            # def get_post(id):
+            #     return Post.query.get(id)
 
-
-# def delete_post(id):
-#     post = Post.query.get(id)
-#     if post:
-#         post.delete()
-#         return True
-#     return False
-
-
-# def get_post(id):
-#     return Post.query.get(id)
-
-
-# def get_all_posts():
-#     return Post.query.all()
+            # def get_all_posts():
+            #     return Post.query.all()
--- app/services/topic_new.py
+++ app/services/topic_new.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from uuid import UUID
 from app.models import Topic_new
 from app import db
+from app.config import Config
 
 payload_platform = {
     "page": 1,
@@ -16,11 +17,13 @@ payload_platform = {
     "order_type": "DESC"
 }
 
-def update_topics_new_from_api(api_url, headers_platform=None,headers_spyder=None, app=None):
+
+def update_topics_new_from_api(api_url, headers_platform=None, headers_spyder=None, app=None):
     with app.app_context():
-        if "platform" in api_url:
+        if Config.PLATFORM_API_URL in api_url:
             try:
-                response = requests.post(api_url, json=payload_platform, headers=headers_platform, verify=False)
+                response = requests.post(
+                    api_url, json=payload_platform, headers=headers_platform, verify=False)
                 if response.status_code == 200:
                     api_data = response.json().get("data", {}).get("items", [])
                     for item in api_data:
@@ -33,31 +36,35 @@ def update_topics_new_from_api(api_url, headers_platform=None,headers_spyder=Non
                         }
                         end_at = item.get("end_at")
                         if end_at:
-                            end_at_datetime = datetime.strptime(end_at, "%Y/%m/%d %H:%M:%S")
+                            end_at_datetime = datetime.strptime(
+                                end_at, "%Y/%m/%d %H:%M:%S")
                             if end_at_datetime < datetime.now():
                                 topic_data["status"] = "deactive"
                             else:
                                 topic_data["status"] = "active"
-                        topic = Topic_new.query.filter_by(id=topic_data['id']).first()
+                        topic = Topic_new.query.filter_by(
+                            id=topic_data['id']).first()
                         if topic:
                             for key, value in topic_data.items():
                                 setattr(topic, key, value)
                         else:
                             topic = Topic_new(**topic_data)
                             db.session.add(topic)
-                        
+
                         db.session.commit()
                         print(f"Updated topic: {topic_data}")
                 else:
-                    print(f"PLATFORM: Failed to fetch objects: {response.status_code}")
+                    print(
+                        f"PLATFORM: Failed to fetch objects: {response.status_code}")
             except requests.exceptions.RequestException as e:
                 print(f"PLATFORM: An error occurred with the request: {e}")
             except Exception as e:
                 print(f"PLATFORM: An unexpected error occurred: {e}")
-        if "spider" in api_url:
+        if Config.SPYDER_API_URL in api_url:
             print("spider")
             try:
-                response = requests.post(api_url, json=payload_platform, headers=headers_spyder, verify=False)
+                response = requests.post(
+                    api_url, json=payload_platform, headers=headers_spyder, verify=False)
                 if response.status_code == 200:
                     api_data = response.json().get("result", {}).get("data", [])
                     for item in api_data:
@@ -68,27 +75,32 @@ def update_topics_new_from_api(api_url, headers_platform=None,headers_spyder=Non
                             "system": "spyder"
                         }
                         if item.get("topic_parent_id"):
-                            topic_data["parent_id"] =  item.get("topic_parent_id"),
-                        else: topic_data["parent_id"] == ""
+                            topic_data["parent_id"] = item.get(
+                                "topic_parent_id"),
+                        else:
+                            topic_data["parent_id"] == ""
                         end_at = item.get("expired_date")
                         if end_at:
-                            end_at_datetime = datetime.strptime(end_at, "%Y/%m/%d %H:%M:%S")
+                            end_at_datetime = datetime.strptime(
+                                end_at, "%Y/%m/%d %H:%M:%S")
                             if end_at_datetime < datetime.now():
                                 topic_data["status"] = "deactive"
                             else:
                                 topic_data["status"] = "active"
-                        topic = Topic_new.query.filter_by(id=topic_data['id']).first()
+                        topic = Topic_new.query.filter_by(
+                            id=topic_data['id']).first()
                         if topic:
                             for key, value in topic_data.items():
                                 setattr(topic, key, value)
                         else:
                             topic = Topic_new(**topic_data)
                             db.session.add(topic)
-                        
+
                         db.session.commit()
                         print(f"Updated topic: {topic_data}")
                 else:
-                    print(f"SPYDER: Failed to fetch objects: {response.status_code}")
+                    print(
+                        f"SPYDER: Failed to fetch objects: {response.status_code}")
             except requests.exceptions.RequestException as e:
                 print(f"SPYDER: An error occurred with the request: {e}")
             except Exception as e:
