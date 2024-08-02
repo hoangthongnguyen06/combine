d361d83aa929d8c8bf3b5ebabfec31520f2eb0a5
hoangthongnguyen06 <nguyenhoangthong06@gmail.com>
2024-08-02 14:31:27 +0700

    fix

--- app/services/topic_day.py
+++ app/services/topic_day.py
@@ -57,56 +57,57 @@ def update_sentiment_topic_from_api(api_url, headers_platform=None, app=None):
     with app.app_context():
         if Config.PLATFORM_API_URL in api_url:
             for id, name, keyword_platform in results_platform:
-                # Lặp qua từng từ khóa trong keyword_platform
-                for keyword in keyword_platform:
-                    payload_platform["topic_ids"] = [keyword]
-                    try:
-                        response = requests.post(api_url, json=payload_platform,
-                                                 headers=headers_platform, verify=False)
-                        if response.status_code == 200:
-                            for details_data in response.json().get("data", []):
-                                if details_data.get("data"):
-                                    for details_data_1 in details_data["data"]:
-                                        extracted_data = {
-                                            "uid": str(uuid.uuid4()),
-                                            "id_topic": id,
-                                            "topic_name": name,
-                                            "date": details_data["date"],
-                                            "sum_of_posts": details_data_1["total_count"],
-                                            "positive_posts": details_data_1["positive_count"],
-                                            "neutral_posts": details_data_1["neutral_count"],
-                                            "negative_posts": details_data_1["negative_count"],
-                                            "system": "platform",
-                                            "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                                            "added_to_json": "0"
-                                        }
-                                        # Xử lý nền tảng của tin bài
-                                        platform_mapping = {
-                                            1: "Báo chí",
-                                            2: "Trang điện tử",
-                                            4: "Blog",
-                                            5: "Diễn đàn",
-                                            8: "Facebook",
-                                            9: "Youtube",
-                                            12: "Tiktok"
-                                        }
-                                        extracted_data["platform"] = platform_mapping.get(details_data_1["source_id"], "Unknown")
+                if keyword_platform:
+                    # Lặp qua từng từ khóa trong keyword_platform
+                    for keyword in keyword_platform:
+                        payload_platform["topic_ids"] = [keyword]
+                        try:
+                            response = requests.post(api_url, json=payload_platform,
+                                                    headers=headers_platform, verify=False)
+                            if response.status_code == 200:
+                                for details_data in response.json().get("data", []):
+                                    if details_data.get("data"):
+                                        for details_data_1 in details_data["data"]:
+                                            extracted_data = {
+                                                "uid": str(uuid.uuid4()),
+                                                "id_topic": id,
+                                                "topic_name": name,
+                                                "date": details_data["date"],
+                                                "sum_of_posts": details_data_1["total_count"],
+                                                "positive_posts": details_data_1["positive_count"],
+                                                "neutral_posts": details_data_1["neutral_count"],
+                                                "negative_posts": details_data_1["negative_count"],
+                                                "system": "platform",
+                                                "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                                                "added_to_json": "0"
+                                            }
+                                            # Xử lý nền tảng của tin bài
+                                            platform_mapping = {
+                                                1: "Báo chí",
+                                                2: "Trang điện tử",
+                                                4: "Blog",
+                                                5: "Diễn đàn",
+                                                8: "Facebook",
+                                                9: "Youtube",
+                                                12: "Tiktok"
+                                            }
+                                            extracted_data["platform"] = platform_mapping.get(details_data_1["source_id"], "Unknown")
 
-                                        top = Topic_day.query.filter_by(uid=extracted_data['uid']).first()
-                                        if top:
-                                            for key, value in extracted_data.items():
-                                                setattr(top, key, value)
-                                            db.session.commit()
-                                        else:
-                                            new_obj = Topic_day(**extracted_data)
-                                            db.session.add(new_obj)
-                                            db.session.commit()
-                        else:
-                            print(f"Failed to fetch objects: {response.status_code}")
-                    except requests.exceptions.RequestException as e:
-                        print(f"An error occurred with the request: {e}")
-                    except Exception as e:
-                        print(f"An unexpected error occurred: {e}")
+                                            top = Topic_day.query.filter_by(uid=extracted_data['uid']).first()
+                                            if top:
+                                                for key, value in extracted_data.items():
+                                                    setattr(top, key, value)
+                                                db.session.commit()
+                                            else:
+                                                new_obj = Topic_day(**extracted_data)
+                                                db.session.add(new_obj)
+                                                db.session.commit()
+                            else:
+                                print(f"Failed to fetch objects: {response.status_code}")
+                        except requests.exceptions.RequestException as e:
+                            print(f"An error occurred with the request: {e}")
+                        except Exception as e:
+                            print(f"An unexpected error occurred: {e}")
         # elif Config.SPYDER_API_URL in api_url:
         #     for topic_id, topic_name in results_spyder:
         #         payload_spyder["category_id"] = topic_id
