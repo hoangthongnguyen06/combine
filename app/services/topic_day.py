33ec208552314cfe0ec699db1e6e2f86f8560320
hoangthongnguyen06 <nguyenhoangthong06@gmail.com>
2024-08-03 14:07:44 +0700

    fix

--- app/services/topic_day.py
+++ app/services/topic_day.py
@@ -111,6 +111,7 @@ def update_sentiment_topic_from_api(api_url, headers=None, app=None):
                     # Lặp qua từng từ khóa trong keyword_spyder
                     for keyword in keyword_spyder:
                         payload_spyder["category_id"] = keyword
+                        print(payload_spyder)
                         try:
                             response = requests.post(api_url, json=payload_spyder, headers=headers, verify=False)
                             response.raise_for_status()
