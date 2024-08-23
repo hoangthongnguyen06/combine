c70e62645b1bcdc4fd9b60c43700ef745728e43d
hoangthongnguyen06 <nguyenhoangthong06@gmail.com>
2024-08-23 23:45:22 +0700

    fix

--- app/services/serverCT.py
+++ app/services/serverCT.py
@@ -34,7 +34,7 @@ def update_server_status(app=None):
                 unit_id_manager = get_unit_id_manager(unit_name)
                 if unit_id_manager:
                     # Cast unit_id_manager to UUID
-                    server = db.session.query(ServerCT).filter_by(unit_id_manager=str(unit_id_manager)).first()
+                    server = db.session.query(ServerCT).filter_by(name=unit_name).first()
                     if server:
                         server.status = status
                         server.last_up = datetime.utcnow()
